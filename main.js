42e4fb2019b37aec0c46bc32ce27666dd6f6118c
endaulster <105703311+endaulster@users.noreply.github.com>
2024-03-26 15:53:47 -0300

    Added delete button functionality to eliminate projects

--- main.js
+++ main.js
@@ -2,11 +2,26 @@
 const listContainer = document.querySelector("[data-list]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
+const deleteListButton = document.querySelector("[data-delete-list-button]");
 
 const LOCAL_STORAGE_LIST_KEY = "task.lists";
-
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
 
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+listContainer.addEventListener("click", e => {
+    if(e.target.tagName.toLowerCase() === "li") {
+        selectedListId = e.target.dataset.listId;
+        saveAndRender();
+    }
+})
+
+deleteListButton.addEventListener("click", e => {
+    lists = lists.filter(list => list.id !== selectedListId);
+    selectedListId = null;
+    saveAndRender();
+})
 
 newListForm.addEventListener("submit", e => {
     e.preventDefault();
@@ -24,6 +39,7 @@ function createList(name) {
 
 function save() {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 }
 
 function saveAndRender() {
@@ -38,6 +54,9 @@ function render () {
         listElement.dataset.listId = list.id;
         listElement.classList.add("list-name");
         listElement.innerText = list.name;
+        if (list.id === selectedListId) {
+            listElement.classList.add("active-option");
+        }
         listContainer.appendChild(listElement);
     })
 }
