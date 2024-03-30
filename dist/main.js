18ee77503c1bd94665f7c241ad030c80ed39cb93
endaulster <105703311+endaulster@users.noreply.github.com>
2024-03-29 21:26:44 -0300

    Added task saving functionality

--- dist/main.js
+++ dist/main.js
@@ -8,6 +8,9 @@ const listDisplayContainer = document.querySelector("[data-list-display-containe
 const listTitleElement = document.querySelector("[data-list-title]");
 const taskContainer = document.querySelector("[data-tasks]");
 const listCountElement = document.querySelector("[data-list-count]");
+const taskTemplate = document.querySelector("#task-template")
+const newTaskForm = document.querySelector("[data-new-task-form]");
+const newTaskInput = document.querySelector("[data-new-task-input]");
 
 const LOCAL_STORAGE_LIST_KEY = "task.lists";
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
@@ -38,8 +41,23 @@ newListForm.addEventListener("submit", e => {
     saveAndRender();
 })
 
+newTaskForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const taskName = newTaskInput.value;
+    if( taskName == null || taskName === "") return
+    const task = createTask(taskName);
+    newTaskInput.value = null;
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks.push(task);
+    saveAndRender();
+})
+
 function createList(name) {
     return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function createTask(name) {
+    return { id: Date.now().toString(), name: name, complete: false }
 }
 
 function save() {
@@ -65,7 +83,23 @@ function render () {
         listTitleElement.innerText = selectedList.name;
         renderTaskCount(selectedList);
         clearElement(taskContainer);
+        renderTasks(selectedList);
     }
+}
+
+function renderTasks(selectedList) {
+
+    selectedList.tasks.forEach(task => {
+        const taskElement = document.importNode(taskTemplate.content, true);
+        const checkbox = taskElement.querySelector("input");
+        checkbox.id = task.id;
+        checkbox.checked = task.complete;
+        const label = taskElement.querySelector("label");
+        label.htmlFor = task.id;
+        label.append(task.name);
+        taskContainer.appendChild(taskElement);
+        
+    })
 }
 
 function renderTaskCount(selectedList){
@@ -95,3 +129,4 @@ function clearElement(element) {
 }
 
 render();
+
